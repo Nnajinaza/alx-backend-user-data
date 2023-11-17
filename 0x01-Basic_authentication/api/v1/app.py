f343542f764fa9d25c9181b3e0ed9788e2fc1de7
Nnajinaza <nnajinaza@gmail.com>
2023-11-17 00:35:54 +0000

    done

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,7 +12,7 @@ import os
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, methods=['GET', 'POST', 'PUT', 'DELETE'])
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
